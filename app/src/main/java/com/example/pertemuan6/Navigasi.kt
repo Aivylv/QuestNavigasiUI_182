ab2d52c16b7d31efb3f39c884a2c8d39a0933af9
aivylv <lyvialarasati@gmail.com>
2025-11-07 19:02:53 +0700

    membuat fungsi pribadi (private fun) untuk mengarahkan navigasi kembali ke rute Formulirku dan menghapus semua layar di atasnya dari back stack (tumpukan) navigasi

--- app/src/main/java/com/example/pertemuan6/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan6/Navigasi.kt
@@ -32,3 +32,9 @@ fun DataApp (
         }
     }
 }
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+) {
+    navController.popBackStack(route = Navigasi.Formulirku.name, inclusive = false)
+}
