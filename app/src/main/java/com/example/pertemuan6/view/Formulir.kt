5eec18fcb68d493e5d2e86cc4224c2e278c9b710
aivylv <lyvialarasati@gmail.com>
2025-11-07 10:42:57 +0700

    membuat dan menampilkan kelompok tombol radio (RadioButton) dengan label teks untuk setiap opsi dalam daftar jenis kelamin (jenisK) dalam tata letak baris horizontal

--- app/src/main/java/com/example/pertemuan6/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -62,3 +62,15 @@ fun FormIsian(
                 .width(250.dp),
                 thickness = Thickness,
                 color = Color.Red)
+            Row{
+                jenisK.forEach {
+                    item ->
+                    Row(verticalAlignment = Alignment.CenterVertically){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
